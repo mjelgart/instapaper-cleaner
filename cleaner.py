72b4ef3d08a3f90c6c49d608821d063f9242ca32
Michael Elgart <mje@michaelelgart.com>
2017-04-26 20:26:44 -0400

    added example config file

--- cleaner.py
+++ cleaner.py
@@ -1,3 +1,14 @@
-import instapaper
+from instapaper import Instapaper as ipaper
+import configparser
+
+
+
+config = configparser.ConfigParser()
+cleanercfg = config.read('instapaper-cleaner.cfg')
+
+
+i = ipaper(cleanercfg['Instapaper OAuth]']['ID'], cleanercfg['Instapaper OAuth]']['Secret'])
+i.login(cleanercfg['Instapaper Login']['email'], cleanercfg['Instapaper Login']['password'])
+
 
 
