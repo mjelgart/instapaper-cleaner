374347df903db4bfe2fb9f4e8f622ed806803d0e
Michael Elgart <mje@michaelelgart.com>
2017-04-27 22:50:11 -0400

    setup.py working

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
-# instapaper-cleaner
+# Instapaper-Cleaner
+
 Cleans out old unread instapaper articles from same website.
 
-Version 0.2.0
+Place the URLs of the websites you'd like instapaper-cleaner to remove.
+Make sure you label this file "targetURLs.txt" once you have added the URLs.
+For now, copy and paste them exactly as they appear. including the "https://" or "http://"
+Remember to also fill out a file named instapaper-cleaner.cfg based on the example: cleaner-example.cfg
+
+
+
+Fill out this configuration file with the indicated values.
+Don't forget to fill out a targetURLs.txt with the URLs you want to clean, based on the targetURLs-example.txt
+
+
 
 Written in Python 3.5
-
-
-@mjelgart
--- setup.py
+++ setup.py
@@ -1,4 +1,19 @@
-from disutils.core import setup
+"""
+setuptools based setup module for instapaper-cleaner.
+
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
 
 desc = """\
 Description
@@ -7,9 +22,36 @@ Description
 Cleans out old unread instapaper articles from same website.
 """
 
-setup(name='instapaper-cleaner',
-      version='0.2.1'
-      py_modules=['cleaner'],
-      author='Michael Elgart',
-      url='https://github.com/mjelgart/instapaper-cleaner'
-      )
+setup(
+    name='instapaper-cleaner',
+    version='0.2.1',
+    
+    description = 'Cleans out old unread instapaper articles from same website.',
+    long_description=long_description,
+     
+    author='Michael Elgart',
+    url='https://github.com/mjelgart/instapaper-cleaner',
+
+    license='Apache',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Hobbyists',
+        'License :: OSI Approved :: Apache License',
+
+        'Programming Language :: Python :: 3',
+    ],
+
+    # What does your project relate to?
+    keywords='instapaper automation cleaning',
+
+    py_modules=["cleaner"],
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['instapaper']
+    
+   )
+
